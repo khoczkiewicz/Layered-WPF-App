3633115b5a9a5de5d11a028cf95c2170ee85173e
Krystian Hoczkiewicz <krystian.hoczkiewicz@medhub.pl>
2018-10-04 03:49:46 +0200

    Update .gitignore

--- Hoczkiewicz.Audi/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "<>", Scope = "namespace", Target = "~N:Hoczkiewicz.Audi.Properties")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1115:Parameter must follow comma", Justification = "<>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1118:Parameter must not span multiple lines", Justification = "<>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "<>")]
